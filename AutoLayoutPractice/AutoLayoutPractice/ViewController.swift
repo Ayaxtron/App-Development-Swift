8092be4255c445d64a9ff8ffdd9944daabd38ebb
Ayax <casarrubias2837@gmail.com>
2018-12-14 18:10:13 -0600

    Update
    chapter 2.9

---
+++ AutoLayoutPractice/AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 14/12/18.
+//  Copyright © 2018 Ayax Alexis Casarrubias Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
