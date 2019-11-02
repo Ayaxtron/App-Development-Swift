f509c915052d099a81d14fccd8822755aaf85236
Ayax <casarrubias2837@gmail.com>
2019-11-02 14:16:25 -0600

    Update

--- ScrollingForm/ScrollingForm/ViewController.swift
+++ ScrollingForm/ScrollingForm/ViewController.swift
@@ -13,8 +13,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        registerForKeyboardNotifications()
     }
-
-
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(keyboardWasShown(_:)),
+                                               name: .UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(keyboardWillBeHidden(_:)),
+                                               name: .UIResponder.keyboardWillHideNotification, object: nil)
+          
+    }
+      
+    @objc func keyboardWasShown(_ notificiation: NSNotification) {
+        guard let info = notificiation.userInfo,
+            let keyboardFrameValue =
+            info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue
+            else { return }
+          
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+          
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0,
+                                         bottom: keyboardSize.height, right: 0.0)
+        UIScrollView.contentInset = contentInsets
+        UIScrollView.scrollIndicatorInsets = contentInsets
+    }
+      
+    @objc func keyboardWillBeHidden(_ notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        UIScrollView.contentInset = contentInsets
+        UIScrollView.scrollIndicatorInsets = contentInsets
+    }
 }
 
