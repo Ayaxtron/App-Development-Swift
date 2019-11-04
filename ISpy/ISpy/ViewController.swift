c5d7729ba392f11e7d6d2e7ad01e3f99f3d95803
Ayax <casarrubias2837@gmail.com>
2019-11-03 21:34:47 -0600

    update

---
+++ ISpy/ISpy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 02/11/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var delegate: UIScrollViewDelegate?
+    }
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
+
