83392e505d77bcce66db01728070c16af16729ba
Ayax <casarrubias2837@gmail.com>
2019-11-07 17:12:16 -0600

    fail ;v

--- EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
@@ -2,7 +2,7 @@
 //  EmojiTableViewController.swift
 //  EmojiDictionary
 //
-//  Created by Ayax Alexis Casarrubias Rodríguez on 04/11/19.
+//  Created by Ayax Alexis Casarrubias Rodríguez on 07/11/19.
 //  Copyright © 2019 Ayax Alexis. All rights reserved.
 //
 
@@ -31,32 +31,13 @@ class EmojiTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
+    
     var emojis: [Emoji] = [
-        Emoji(symbol: "😀", name: "Grinning Face",
-              description: "A typical smiley face.", usage: "happiness"),
-    Emoji(symbol: "😕", name: "Confused Face",
-    description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"),
-    Emoji(symbol: "😍", name: "Heart Eyes",
-    description: "A smiley face with hearts for eyes.",
-    usage: "love of something; attractive"),
-    Emoji(symbol: "👮", name: "Police Officer",
-    description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),
-    Emoji(symbol: "🐢", name: "Turtle", description:
-    "A cute turtle.", usage: "Something slow"),
-    Emoji(symbol: "🐘", name: "Elephant", description:
-    "A gray elephant.", usage: "good memory"),
-    Emoji(symbol: "🍝", name: "Spaghetti",
-    description: "A plate of spaghetti.", usage: "spaghetti"),
-    Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),
-    Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),
-    Emoji(symbol: "📚", name: "Stack of Books",description: "Three colored books stacked on each other.",
-    usage: "homework, studying"),
-    Emoji(symbol: "💔", name: "Broken Heart",
-    description: "A red, broken heart.", usage: "extreme sadness"), Emoji(symbol: "💤", name: "Snore",
-    description:"Three blue \'z\'s.", usage: "tired, sleepiness"),
-    Emoji(symbol: "🏁", name: "Checkered Flag",
-    description: "A black-and-white checkered flag.", usage: "completion")]
+        Emoji(symbol: "😀", name: "Grinning Face", description: "A typical smiley face.", usage: "happiness"), Emoji(symbol: "😕", name: "Confused Face", description: "A confused, puzzled face.", usage: "unsure what to think; displeasure"), Emoji(symbol: "😍", name: "Heart Eyes", description: "A smiley face with hearts for eyes.",usage: "love of something; attractive"), Emoji(symbol: "👮", name: "Police Officer",description: "A police officer wearing a blue cap with a gold badge.", usage: "person of authority"),Emoji(symbol: "🐢", name: "Turtle", description: "A cute turtle.", usage: "Something slow"),Emoji(symbol: "🐘", name: "Elephant", description:"A gray elephant.", usage: "good memory"),Emoji(symbol: "🍝", name: "Spaghetti",description: "A plate of spaghetti.", usage: "spaghetti"),Emoji(symbol: "🎲", name: "Die", description: "A single die.", usage: "taking a risk, chance; game"),Emoji(symbol: "⛺️", name: "Tent", description: "A small tent.", usage: "camping"),Emoji(symbol: "📚", name: "Stack of Books",description: "Three colored books stacked on each other.",usage: "homework, studying"),Emoji(symbol: "💔", name: "Broken Heart",description: "A red, broken heart.", usage: "extremesadness"), Emoji(symbol: "💤", name: "Snore",description:"Three blue \'z\'s.", usage: "tired, sleepiness"),Emoji(symbol: "🏁", name: "Checkered Flag",description: "A black-and-white checkered flag.", usage:"completion")]
+    
+    
+    
+     
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
@@ -113,4 +94,3 @@ class EmojiTableViewController: UITableViewController {
     */
 
 }
-
