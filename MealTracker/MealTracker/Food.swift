4e7e7210de48ba45d2a557f37b7a3d4f58edf2dd
Ayax <casarrubias2837@gmail.com>
2019-11-07 18:02:49 -0600

    another fail

---
+++ MealTracker/MealTracker/Food.swift
@@ -0,0 +1,16 @@
+//
+//  Food.swift
+//  MealTracker
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 07/11/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import Foundation
+
+struct Food{
+    var name : String
+    var description : String
+    
+    
+}
---
+++ MealTracker/MealTracker/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  MealTracker
+//
+//  Created by Ayax Alexis Casarrubias Rodríguez on 07/11/19.
+//  Copyright © 2019 Ayax Alexis. All rights reserved.
+//
+
+import Foundation
+
+struct Meal{
+    var name : String
+    var food : Food
+}
